93c658e5ff9d49cca1eb08e1623aa82a32e110a8
Laura Obare <156826263+LorraObare@users.noreply.github.com>
2026-07-13 21:25:01 +0300

    Added introduction to JavaScript concepts
    This file provides an overview of JavaScript, including its execution types, frontend vs backend distinctions, and client-server architecture.

---
+++ introduction-to-JavaScript.js
@@ -0,0 +1,26 @@
+/*An algorithm is just a generic set of intsructions , not written for any specific programming language.
+
+-A Program has code that is syntactically valid according to the rules of a specific programming language.
+-JavaScript is an interpreted programming language used to code the interactivity on web pages or web applications as Frontedn Development.
+
+Execution type → how does the program run?
+
+Execution Type	How it works	Examples
+1.Compiled	Entire code is translated to machine code (bits) before running.	C, C++, Rust, Go
+2.Interpreted	Code is read and executed (interpreted) line by line, not compiled/translated before.	JavaScript, Python, Ruby
+3.Hybrid	Code is first compiled to bytecode, then interpreted or JIT(just-in-time)-compiled at runtime	Java, C#, Kotlin
+
+Frontend vs Backend → where does the program run?
+
+Part	Runs on	Used for	Examples
+Frontend	User’s browser or device	What the user sees and interacts with	HTML, CSS, JavaScript
+Backend	Server (remote machine)	Logic, data, storage, authentication	Java, Python, C#, PHP, Node.js, Go
+
+-An example of an interpreter program is a web browser. Thats the frontend side where JavaScripr runs. So whenever a program in JavaScript needs some heavy logic or access to a resource in the backedn, like a database, it might make a call to amothet program runnign on the backedn side. These tow develoment parts together can represent a Client-Server Architecture.
+
+Client Server Architecture
+-Client-side (Frontend) : Controls what you see in the browser and handles user's interactivity. Also requests services and data from the server- side.
+-Server-side (Backend) : Responds to requests from client-side by also accessing other services or resources, like database.
+
+-A function is like a command or block of commands to perform a small task, as in our rice recipe. Note: A string (text) is always represented within quotation marks("")
+-As we know, JavaScript cannot function on its own in a web browser. It needs to be embedded in a web page, written in a markup language called HTML.
