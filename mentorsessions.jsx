bbea384d351c6658e34ead04d140c3ac418b371d
Laura Obare <156826263+LorraObare@users.noreply.github.com>
2026-07-22 12:51:55 +0300

    Add JavaScript variable and type examples
    Added examples demonstrating variable declaration, reassignment, and type checking in JavaScript.

---
+++ mentorsessions.jsx
@@ -0,0 +1,34 @@
+// //2. Declare a variable one with ‘let’ keyword and another with ‘const’ keyword 
+// let myVar= "Hello World";
+// const myConst = "I love JavaScript";
+
+// console.log(myVar);
+// console.log(myConst);
+
+// // 3.Try and reassign it and log result to console
+
+// // myVar = (Hi);
+// // myConst = (I love React);
+
+// // 4.Declare three variables one with let , one with var, one with const
+
+// let first = "Laura";
+// var second ="Osanya";
+// const third ="Obare";
+
+// // 5. Assign each a string value then concatenate all three strings into a single string and log the result to console
+// console.log(first + " " + second + " " + third);
+
+/* 6. Declare a variable with a number, one with a string, and one with a boolean
+Use typeof to log their data types */
+
+const age = 21;
+let myName ="Laura";
+let isGirl= true;
+
+console.log(typeof (age));
+console.log(typeof (myName));
+console.log(typeof (isGirl));
+
+// Today I learnt about typeof and why it is necessary and even how to use it.
+//The typeof operator in JavaScript is a unary operator that returns a string indicating the data type of an unevaluated operand. It is a fundamental tool for runtime type checking, debugging, and safely identifying data types before performing operations on variables. 
