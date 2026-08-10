f9ac2a73cd75b53b1b70aef1a9c5ab9a0d3f0a00
Laura Obare <156826263+LorraObare@users.noreply.github.com>
2026-08-10 16:31:13 +0300

    Document JavaScript operators and expressions
    Added comments explaining operators and expressions in JavaScript.

---
+++ Operators and Expressions.jsx
@@ -0,0 +1,39 @@
+//In coding, values dont just sit there. They can be combined, compared, changed. Values can e some sort of information like numbers, texts, lists.These are usually used in a calculation, in a function, or stored in a variable. The symbols used to perfomr these actions on values are called Operators.
+//Operators in JavaScript let you
+
+//Assign a value ( ==, += ,-== ,*=, /) %=, **=)
+//The result is assigned to a variable.
+
+//compare value (> , <, >= ,<= , == , != , === ,!==)
+//The result is either true or false.
+
+//Perform arithmetic( +, - , *, /, %, **)
+//The result is another value of the same data type.
+
+//Work with logic ( &&, ||, !)
+//The result is either true or false and is used to make a decision based on a condition.
+
+//These operators are called:
+&& :AND
+||:OR
+!:NOT (negation)
+
+//It is important to note that the operator + can be used with strings as well as numeric data data types.
+//When used with strings, the strings become concatenated, meaning they are connected. For example, "Hello" + "World!" would result in "Hello World!". You can also concatenate strings and numbers, which are then converted into strings. For example, "Hello" + "World" + 2025 woud result in "Hello World! 2025".
+
+//Example
+const isActive = true; //Assignment operator
+let a = 5 + 3; //Arithmetic operator -> a equals 8
+let b = (a > 5); //Comparison operator -> b equals true
+let age = a * 2 - 1; //Mixed operators -> age equals 15
+let isAdult = age >= 18 &&isActive; //Logical Operator -> isAdult equals false.
+
+//Basic Arithmetic Operators 
+// + Addition
+// - Subtraction
+// * Multiplication
+// / Division
+// % Modulus
+// ++ Increment
+// -- Decrement
+// **Exponentiation
